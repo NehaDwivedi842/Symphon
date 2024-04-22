e1a9134cc74179164de395f421b99bfaf0691f0a
NehaDwivedi842 <109571688+NehaDwivedi842@users.noreply.github.com>
2024-04-22 11:00:23 +0530

    Update coin_de.py

--- coin_de.py
+++ coin_de.py
@@ -16,10 +16,10 @@ def main():
     num_cavities = st.number_input("Enter the number of cavities:", min_value=1, value=1)
     tons_per_inch_sq = st.number_input("Enter tons per inch square:", min_value=0.1, value=1.0, step=0.1)
 
-    webrtc_ctx = webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+    webrtc_ctx = webrtc_streamer(key="example", video_processor_factory=VideoTransformer)
 
-    if webrtc_ctx.video_transformer:
-        result_image = webrtc_ctx.video_transformer.result
+    if webrtc_ctx.video_processor:
+        result_image = webrtc_ctx.video_processor.transformed_frame
         if result_image is not None:
             st.image(result_image, channels="BGR", use_column_width=True)
 
